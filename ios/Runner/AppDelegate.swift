df87d824d15c67741ae781f12f1a375d55a89e20
enes <enes.agir.7@gmail.com>
2025-11-21 16:04:42 +0100

    ios settings

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,9 +9,9 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     // 2. Get the API Key from the environment variables
-    let googleMapsApiKey = (Bundle.main.object(forInfoDictionaryKey: "GoogleMapsApiKey") as? String)!
+//    let googleMapsApiKey = (Bundle.main.object(forInfoDictionaryKey: "GoogleMapsApiKey") as? String)!
     // 3. Provide the API Key to GMSServices
-    GMSServices.provideAPIKey(googleMapsApiKey)
+    GMSServices.provideAPIKey("your google maps api key")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
